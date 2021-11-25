0f2b6af30893dd1fe097eabf9d529f994339fa36
mhio <37205532+mhio@users.noreply.github.com>
2021-11-25 04:57:56 +0000

    word

--- shares.go
+++ shares.go
@@ -21,7 +21,7 @@ func main() {
 
 func root() error {
     // Args
-    total_keys_flag := flag.Int("k", 0, "Number of keys go generate")
+    total_keys_flag := flag.Int("k", 0, "Number of keys to generate")
     threshold_keys_flag := flag.Int("t", 0, "Threshold # of keys to recreate")
     flag_key_length := flag.Int("l", 64, "Length of generated key")
     flag.Parse()
